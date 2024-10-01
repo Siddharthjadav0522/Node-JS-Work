044e650cf3793c18f8da6a5041ad4f2f7367d923
Siddharth Jadav <f477siddharth@gmail.com>
2024-10-01 20:11:13 +0530

    ok

--- Movie API/route/movie.js
+++ Movie API/route/movie.js
@@ -1,74 +1,17 @@
 const express = require("express");
 const route = express.Router();
 const movieModel = require("../model/movie");
+const { createData, readData, readOneData, updateData, deleteData } = require("../controller/movie");
 
 
-route.post("/create", async (req, res) => {
-    try {
-        let { fullname, director, cast, genres, language, rating } = req.body;
-        let findMovie = await movieModel.findOne({ fullname });
-        if (findMovie) {
-            res.status(409).json({
-                msg: "Movie alredy exist"
-            })
-        } else {
-            let movie = await movieModel.create({
-                fullname, director, cast, genres, language, rating
-            });
-            res.status(201).json(movie);
-        }
-    } catch (error) {
-        res.send(error.message)
-    }
-});
+route.post("/create", createData);
 
-route.get("/read", async (req, res) => {
-    try {
-        let allData = await movieModel.find();
-        res.send(allData);
-    } catch (error) {
-        res.send(error.message);
-    }
-});
+route.get("/read", readData);
 
-route.get("/readOne/:id", async (req, res) => {
-    try {
-        let id = req.params.id;
-        let data = await movieModel.findById(id);
-        res.send(data);
-    } catch (error) {
-        res.send(error.message);
-    }
-});
+route.get("/readOne/:id", readOneData);
 
-route.put("/update/:id", async (req, res) => {
-    try {
-        let id = req.params.id;
-        let movie = await movieModel.findOne({ _id: id });
-        if (!movie) {
-            return res.status(400).send("Movie not found");
-        }
-        let { fullname, director, cast, genres, language, rating } = req.body;
-        let data = await movieModel.findByIdAndUpdate(id, { fullname, director, cast, genres, language, rating });
-        res.send(data);
-    } catch (error) {
-        res.send(error.message);
-    }
-});
+route.put("/update/:id", updateData);
 
-route.delete("/delete/:id", async (req, res) => {
-    try {
-        let id = req.params.id;
-        let movie = await movieModel.findOne({ _id: id });
-        if (!movie) {
-            return res.status(400).send("Movie not found");
-        }
-        let deleteMovie = await movieModel.findByIdAndDelete(id);
-        console.log(deleteMovie);
-        res.send("movie deleted");
-    } catch (error) {
-        res.send(error.message);
-    }
-})
+route.delete("/delete/:id", deleteData)
 
 module.exports = route;
