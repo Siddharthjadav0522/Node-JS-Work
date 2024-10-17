3fe25d4f436991e2e66fade71ec25107d5057f53
Siddharth Jadav <f477siddharth@gmail.com>
2024-10-17 12:47:57 +0530

    ok

--- Movie_and_blog_api/controller/user.js
+++ Movie_and_blog_api/controller/user.js
@@ -1,41 +1,53 @@
 const sendOtp = require("../config/email_config");
 const User = require("../model/user");
+const bcrypt = require('bcrypt');
 
 const otpStore = {};
 
 const register = async (req, res) => {
     let { username, email, password, otp } = req.body;
-    // console.log(otp);
-    // console.log(otpStore.email.otp , "--" ,Date.now() - otpStore.email.time);
 
     if (!otpStore.email) {
         return res.status(400).json({ msg: "OTP not sent or expired" });
     }
-    if (otpStore.email.otp !== otp && (Date.now() - otpStore.email.time >= 120000)) {
-        return res.status(400).send("email verification fail");
+
+    if (otpStore.email.otp !== otp || (Date.now() - otpStore.email.time >= 60000)) {
+        return res.send("email verification fail");
     };
     delete otpStore.email;
-    console.log(otpStore);
+
     let findUser = await User.findOne({ email });
     if (findUser) {
-        res.status(409).json({
+        return res.status(409).json({
             msg: "user alredy exist"
         })
-    } else {
-        let user = await User.create({
-            username, email, password
-        });
-        res.status(201).json(user);
     }
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        let user = await User.create({
+            username,
+            email,
+            password: hashedPassword,
+        });
+        res.status(201).json({ user });
+
+    } catch (error) {
+        res.status(500).json({ msg: "Server error", error });
+    }
+
 };
+
 const login = async (req, res) => {
     let { username, email, password } = req.body;
     let findUser = await User.findOne({ email });
+    if (!findUser) {
+        return res.send("envalid user email")
+    }
     let id = findUser._id;
-
-
     if (findUser && findUser.username === username) {
-        if (findUser.password === password) {
+        if (bcrypt.compareSync(password, findUser.password)) {
             req.session.user = {
                 id
             }
